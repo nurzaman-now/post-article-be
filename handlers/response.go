4e114ea3b3822f25f9c9db0907d63f4abd732bee
nurzaman-now <imannj69@gmail.com>
2026-08-09 05:40:02 +0700

    feat(handler): add metadata support to response

--- handlers/response.go
+++ handlers/response.go
@@ -5,22 +5,32 @@ import "github.com/gin-gonic/gin"
 // APIResponse adalah struktur standar response untuk REST API
 type APIResponse struct {
 	Status  string `json:"status"`
+	Meta    any    `json:"meta"`
 	Data    any    `json:"data"`
 	Message string `json:"message"`
 }
 
 // ResponseSuccess mengirim HTTP 200 OK dengan format response sukses yang seragam
-func ResponseSuccess(ctx *gin.Context, data any, message string) {
-	ctx.JSON(200, APIResponse{
+func ResponseSuccess(ctx *gin.Context, meta any, data any, message string, code ...int) {
+	statusCode := 200
+	if len(code) > 0 {
+		statusCode = code[0]
+	}
+	ctx.JSON(statusCode, APIResponse{
 		Status:  "success",
 		Data:    data,
 		Message: message,
+		Meta:    meta,
 	})
 }
 
 // ResponseError mengirim HTTP 400 Bad Request dengan format response error yang seragam
-func ResponseError(ctx *gin.Context, data any, message string) {
-	ctx.JSON(400, APIResponse{
+func ResponseError(ctx *gin.Context, data any, message string, code ...int) {
+	statusCode := 400
+	if len(code) > 0 {
+		statusCode = code[0]
+	}
+	ctx.JSON(statusCode, APIResponse{
 		Status:  "error",
 		Data:    data,
 		Message: message,
